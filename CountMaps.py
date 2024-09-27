d10e981a24f0c1f3d1a5f8a94bae0f82d95809a4
CraftsmanCode <craftsmancode1@gmail.com>
2024-09-27 17:50:26 -0400

    feat: modifying code on line 11

--- CountMaps.py
+++ CountMaps.py
@@ -8,7 +8,7 @@ from tensorflow.python.keras.models import Sequential, load_model
 print("TensorFlow version:", tf.__version__)
 print("Keras version:", tf.keras.__version__)
 from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense #Línea con error (requiere cambio)
-from keras.preprocessing.image import img_to_array #Línea con error (requiere cambio)
+from tensorflow.python.keras.preprocessing.image import img_to_array #Línea con error (requiere cambio)
 
 
 # Creación del modelo simplificado
